948cbbb8bda90cc322072f3f792e8384a12f6609
Padmanabh Belavadi <pro.paddusonu@gmail.com>
2023-02-26 14:35:00 +0530

    complete about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import Intro from './Intro'
+import WhatWeDo from './WhatWeDo'
+import WhyChooseUs from './WhyChooseUs'
 
 const About = () => {
   return (
     <Layout>
-      <div>About</div>
+      <div>
+      <Intro/>
+      <WhatWeDo/>
+      <WhyChooseUs/>
+      </div>
+
+
     </Layout>
   )
 }
